2f6f99a788496922b880154e8f24badb76174899
Fan Liu <liufan321@gmail.com>
2014-12-25 02:15:08 +0800

    翻译 Reachability.h

--- Reachability/Reachability.h
+++ Reachability/Reachability.h
@@ -51,9 +51,9 @@
 
 
 typedef enum : NSInteger {
-	NotReachable = 0,
-	ReachableViaWiFi,
-	ReachableViaWWAN
+	NotReachable = 0,       // 没有连接
+	ReachableViaWiFi,       // 通过 Wi-Fi 连接
+	ReachableViaWWAN        // 通过 2G/3G/4G 连接
 } NetworkStatus;
 
 
@@ -64,34 +64,53 @@ extern NSString *kReachabilityChangedNotification;
 
 /*!
  * Use to check the reachability of a given host name.
+ * <br />检测能否连接到指定的主机
  */
 + (instancetype)reachabilityWithHostName:(NSString *)hostName;
 
 /*!
  * Use to check the reachability of a given IP address.
+ * <br />检测能否连接到指定的 IP 地址
  */
 + (instancetype)reachabilityWithAddress:(const struct sockaddr_in *)hostAddress;
 
 /*!
  * Checks whether the default route is available. Should be used by applications that do not connect to a particular host.
+ * <br />检测默认的路由器是否可用，由应用程序使用而不是连接到特定的主机
  */
 + (instancetype)reachabilityForInternetConnection;
 
 /*!
  * Checks whether a local WiFi connection is available.
+ * <br />检测本地 Wi-Fi 连接是否可用
  */
 + (instancetype)reachabilityForLocalWiFi;
 
 /*!
  * Start listening for reachability notifications on the current run loop.
+ * <br />开始在当前运行循环监听网络连接通知
  */
 - (BOOL)startNotifier;
+
+/*!
+ *  停止监听器
+ */
 - (void)stopNotifier;
 
+/*!
+ *  当前连接状态
+ *
+ *  @return 网络状态
+ */
 - (NetworkStatus)currentReachabilityStatus;
 
 /*!
  * WWAN may be available, but not active until a connection has been established. WiFi may require a connection for VPN on Demand.
+ * <br />是否需要连接
+ * <ul>
+ * <li>WWAN 在连接建立之前不能使用</li>
+ * <li>Wi-Fi 通过 VPN 连接</li>
+ * </ul>
  */
 - (BOOL)connectionRequired;
 
